22dd9a684af71f277b2e869ba98805fae1fb1704
Vladimir Denisov <v.denisov@msk-wire-v_denisov-v.denisov.mail.msk>
2016-10-19 12:48:52 +0300

    add_verbose

--- survey/models.py
+++ survey/models.py
@@ -35,6 +35,9 @@ class Country(models.Model):
         max_length=5, unique=True, null=True, blank=True, verbose_name="короткое название страны"
     )
 
+    def __str__(self):
+        return self.name
+
 
 class Wine(models.Model):
     color = models.CharField(max_length=10)
