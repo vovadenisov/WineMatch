4748a14e8845d7b75d5c1a66cdcf8264ac35ff7f
Diuidina Anastasia <a.dyudina@corp.mail.ru>
2016-10-21 06:09:37 +0000

    blur added

--- survey/admin.py
+++ survey/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from survey.models import Country, Wine
+from survey.models import Country, Wine, Question
 
 admin.site.register(Country)
 admin.site.register(Wine)
+admin.site.register(Question)
